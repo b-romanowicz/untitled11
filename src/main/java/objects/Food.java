49f1cd933450a2223d7fafaf6b9f6f3d74f389ce
jakkard <jakkard@onet.pl>
2020-06-04 15:47:43 +0200

    s

--- src/main/java/objects/Food.java
+++ src/main/java/objects/Food.java
@@ -1,15 +1,6 @@
 package main.java.objects;
 
-import main.java.objects.Position;
-
 public abstract class Food implements IObject{
-
-	private Position position;
-	
-	public Position getPosition()
-	{
-		return position;
-	}
 	
 	public abstract void feed();
 	
