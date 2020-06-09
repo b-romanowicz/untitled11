19c56fb4ecec2c1fc8c11bdffc39972acafe13e3
krzysiek <wrzysztofk999@gmail.com>
2020-06-09 17:51:29 +0200

    save&event mk1

--- src/main/java/SaveToCSV.java
+++ src/main/java/SaveToCSV.java
@@ -1,3 +1,30 @@
+package sample;
+
+import javax.swing.*;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
+
 public class SaveToCSV {
-    public static void saveFile(String fileName, int [][][] data){}
+
+    private String filePatch="akwarium.txt";
+    String a ;
+
+    public static void saveFile(String a,String filePatch) {
+
+        try {
+            FileWriter fileWriter = new FileWriter(filePatch, true);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            PrintWriter printWriter = new PrintWriter(bufferedWriter);
+
+            printWriter.println(a);//tu co chemy wpisywac do txt
+            printWriter.flush();
+            printWriter.close();
+
+            //JOptionPane.showMessageDialog(null, "Zapisano");
+        } catch (Exception E) {
+            JOptionPane.showMessageDialog(null, "Nie zapisano");
+        }
+    }
 }
